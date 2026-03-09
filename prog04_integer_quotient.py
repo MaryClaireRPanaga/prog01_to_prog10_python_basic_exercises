d49654d48ade8fcd1c85f098a67da3b504b44def
MaryClaireRPanaga <maryclaireremigiopanaga@gmail.com>
2026-03-09 19:08:39 +0800

    used_floor_division_to_remove_decimal_from_quotient

---
+++ prog04_integer_quotient.py
@@ -0,0 +1,6 @@
+# Prog04: Create a program that ask user to input 2 numbers. Print the quotient of the two numbers without the decimal point
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+#Use // for integer division (floor division)
+quotient = num1 //num2
+print(quotient)
